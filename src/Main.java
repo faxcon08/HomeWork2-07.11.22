d05ed69687e02f60c33ab8116cc42209ebaf0ceb
faxcon08 <faxcon-08@mail.ru>
2022-11-07 22:27:49 +0300

    Third task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+          final String ANSI_RED = "\u001B[31m"; // Красный цвет в консоле
+          final String ANSI_RESET = "\u001B[0m";// Черный цвет в консоле
+
+      /*First task*/
+        System.out.println(ANSI_RED + "First task" + ANSI_RESET);
+            var dog = 8.0;
+            var cat = 3.6;
+            var paper = 763789;
+            System.out.println("dog="+dog+" cat="+cat+" paper="+paper);
+        /*End First task*/
+
+        // Second task
+        System.out.println(ANSI_RED + "Second task" + ANSI_RESET);
+             dog = dog +4;
+             cat = cat +4;
+             paper = paper +4;
+            System.out.println("New Values: dog="+dog+" cat="+cat+" paper="+paper);
+
+        // Third task
+        System.out.println(ANSI_RED + "Third task" + ANSI_RESET);
+            dog = dog - 3.5;
+            cat = cat - 1.6;
+            paper = paper - 7639;
+            System.out.println("New Values: dog="+dog+" cat="+cat+" paper="+paper);
     }
 }
